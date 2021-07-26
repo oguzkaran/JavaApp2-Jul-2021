e4cf5aa338f0ec7a4f84e3bde0506312c4879f1f
Oğuz Karan <oguzkaran@csystem.org>
2021-07-26 15:27:45 +0300

    JavaApp2-Jul-2021

---
+++ src/Projects/GenericApp/src/main/java/org/csystem/app/App.java
@@ -0,0 +1,10 @@
+package org.csystem.app;
+
+import org.csystem.util.console.Console;
+
+public class App {
+    public static void main(String[] args)
+    {
+        Console.writeLine("Hello, World!");
+    }
+}
