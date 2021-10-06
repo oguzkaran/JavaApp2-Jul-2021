0560db23d16b24f012c023cd4f622c8e99ac46ad
Oğuz Karan <oguzkaran@csystem.org>
2021-10-06 20:35:06 +0300

    Update

---
+++ src/Projects/008-UDP/005-SampleSendRandomNumberPeriodically/Receiver/src/main/java/org/csystem/application/udp/receiver/configuration/ExecutorServiceConfig.java
@@ -0,0 +1,17 @@
+package org.csystem.application.udp.receiver.configuration;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Configuration
+public class ExecutorServiceConfig {
+    @Bean
+    public ExecutorService getSingleThread()
+    {
+        return Executors.newSingleThreadExecutor();
+    }
+}
