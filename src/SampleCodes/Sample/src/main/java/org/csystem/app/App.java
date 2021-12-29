510fdffe24af406d26c65096b8d3a4305c2983f6
Oğuz Karan <oguzkaran@csystem.org>
2021-12-29 20:41:27 +0300

    Update

--- src/SampleCodes/Sample/src/main/java/org/csystem/app/App.java
+++ src/SampleCodes/Sample/src/main/java/org/csystem/app/App.java
@@ -1,48 +1,47 @@
 /*----------------------------------------------------------------------------------------------------------------------
-    Aşağıdaki örnekte rpg1'e ilişkin işlem bittikten sonra rpg2'ye ilişkin işlem yapılır
+    File sınıfının createTempFile metodu
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
-import org.csystem.util.concurrent.random.RandomPasswordGenerator;
 import org.csystem.util.console.CommandLineArgsUtil;
 import org.csystem.util.console.Console;
+import org.csystem.util.string.StringUtil;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.Random;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executors;
 
 class App {
     public static void main(String[] args)
     {
-        RandomGeneratorApp.run(args);
+        CommandLineArgsUtil.checkForLengthEqual(args, 1, "Wrong number of arguments", 1);
+
+        File file;
+        try (var bw = Files.newBufferedWriter(Path.of((file = File.createTempFile("csd", ".txt", new File("."))).getAbsolutePath()),
+                StandardCharsets.UTF_8, StandardOpenOption.CREATE)) {
+            Console.writeLine("Path:%s", file.getAbsolutePath());
+            var count = Integer.parseInt(args[0]);
+            var random = new Random();
+
+            for (int i = 0; i < count; ++i) {
+                bw.write(StringUtil.getRandomTextTR(random, 10) + "\r\n");
+                bw.flush();
+            }
+
+            Console.writeLine("Length:%d", file.length());
+
+            Console.readLine();
+        }
+        catch (NumberFormatException ignore) {
+            Console.Error.writeLine("Invalid count value");
+        }
+        catch (IOException ex) {
+            Console.Error.writeLine(ex.getMessage());
+        }
+        Console.readLine();
     }
 }
-
-class RandomGeneratorApp {
-    public static void run(String [] args)
-    {
-        CommandLineArgsUtil.checkForLengthEqual(args, 4, "Wrong number of arguments", 1);
-        var threadPool = Executors.newCachedThreadPool();
-
-        try {
-            var rpg1 = new RandomPasswordGenerator(Integer.parseInt(args[0]), Integer.parseInt(args[1]),
-                    Integer.parseInt(args[2]), Short.parseShort(args[3]), new Random(), threadPool);
-
-            var rpg2 = new RandomPasswordGenerator(Integer.parseInt(args[0]), Integer.parseInt(args[1]),
-                    Integer.parseInt(args[2]), Short.parseShort(args[3]), new Random(), threadPool);
-
-            rpg1.run();
-            rpg2.run();
-
-            Console.writeLine("Size:%d", rpg1.awaitAndGet().size());
-            Console.writeLine("Size:%d", rpg2.awaitAndGet().size());
-        }
-        catch (Throwable ex) {
-            ex.printStackTrace();
-        }
-        finally {
-            threadPool.shutdown();
-        }
-    }
-}
-
