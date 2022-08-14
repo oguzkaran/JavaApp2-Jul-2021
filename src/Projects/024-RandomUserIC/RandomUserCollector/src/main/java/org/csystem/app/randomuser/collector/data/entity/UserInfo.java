021621dd07c2defbdaaa4a30f65d713dfe14ae61
Oğuz Karan <oguzkaran@csystem.org>
2022-08-14 22:36:30 +0300

    Update

---
+++ src/Projects/024-RandomUserIC/RandomUserCollector/src/main/java/org/csystem/app/randomuser/collector/data/entity/UserInfo.java
@@ -0,0 +1,10 @@
+package org.csystem.app.randomuser.collector.data.entity;
+
+
+import java.io.Serializable;
+
+
+public class UserInfo implements Serializable {
+    private static final long serialVersionID = 1L;
+    //...
+}
